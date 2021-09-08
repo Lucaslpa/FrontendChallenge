9fe7ae465e70da4a45da6b564502273854056310
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-09-08 16:04:34 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "standard"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    quotes: "off",
+    "comma-dangle": "off",
+    semi: "off",
+    "react/react-in-jsx-scope": "off",
+    "space-before-function-paren": "off",
+    "no-undef": "off",
+  },
+};
