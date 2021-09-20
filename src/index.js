8d9c166641517ea53a329825de3fc0d6b9f9b77f
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-09-20 00:27:15 -0300

    Add routes and Contexts components to index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 import "./index.css";
 import App from "./App";
 import { PatientsContext } from "./Contexts/Patients";
 import { PatientModalContext } from "./Contexts/PatientModal";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+const Routes = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <App />
+      </Route>
+      <Route exact path="/patient/:uuid">
+        <App />
+      </Route>
+    </Switch>
+  </Router>
+);
+
+const Contexts = ({ children }) => (
+  <PatientsContext>
+    <PatientModalContext>{children}</PatientModalContext>
+  </PatientsContext>
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <PatientsContext>
-      <PatientModalContext>
-        <Router>
-          <Switch>
-          <Route exact path="/">
-              <App />
-            </Route>
-            <Route exact path="/patient/:uuid">
-              <App />
-            </Route>
-          </Switch>
-        </Router>
-      </PatientModalContext>
-    </PatientsContext>
+    <Contexts>
+      <Routes />
+    </Contexts>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+Contexts.propTypes = {
+  children: PropTypes.any,
+};
